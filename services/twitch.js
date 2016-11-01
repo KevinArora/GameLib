427a270490d7c08c0356d983145eb0040631c92b
Kevin Arora <dzabuza@gmail.com>
2016-11-01 09:12:59 -0400

    small fires

--- services/twitch.js
+++ services/twitch.js
@@ -1,12 +1,10 @@
 const fetch = require('node-fetch');
 
-const API_URL = 'https://api.twitch.tv/kraken/search/streams?q=Pokemon&limit=100&offset=0&client_id=siajlizz3jbodyg2a3fss07ycc2c9rg';
-
 
 function twitchSearch(req, res, next) {
-const game = req.query.game;
-console.log(game)
-  fetch(`https://api.twitch.tv/kraken/search/streams?q=${game}&limit=100&offset=0&client_id=siajlizz3jbodyg2a3fss07ycc2c9rg`)
+  const game = req.query.game;
+  console.log(game)
+  fetch(`https://api.twitch.tv/kraken/search/streams?q=${game}&limit=100&offset=0&client_id=${API_ID}`)
   .then(r => r.json())
   .then((data) => {
     res.twitch = data;
